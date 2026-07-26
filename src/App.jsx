c027b865255316ea8daaefecb70aed6a61803e7e
Kel Siach <kelsiach@Kels-MacBook-Pro.local>
2026-07-26 19:04:05 +0800

    Completed Part 8 - add Jotai flash messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'wouter';
 
+import FlashMessage from './FlashMessage';
 import Navbar from './Navbar';
 import HomePage from './HomePage';
 import ProductPage from './ProductPage';
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <FlashMessage />
 
       <Switch>
         <Route path="/" component={HomePage} />
